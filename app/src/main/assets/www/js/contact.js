539fab6fed6eefbea2b1a91b99dea15a6ba6e5fd
t-nakahata <t_nakahata@RJC7052.r-jc.local>
2016-01-22 17:15:58 +0900

    refs #8251アドレス 連絡先アプリ連係部品

--- app/src/main/assets/www/js/contact.js
+++ app/src/main/assets/www/js/contact.js
@@ -1,30 +1,4 @@
 document.addEventListener('deviceready', function() {
-<<<<<<< Updated upstream
-    // 連絡先機能を実装してください。
-}, false);
-
-function doAction(){
-    var fstr = document.getElementById('input').value;
-    var opt = new ContactFindOptions();
-    opt.filter = fstr;
-    var flds = ["displayName","emails"];
-    var contact = navigator.contacts.find(flds,onSuccess,onError,opt);
-}
-
-function onSuccess(contacts) {
-    var res = "<ol>";
-    for (var i=0; i < contacts.length; i++) {
-        res += '<li class="mail_info">' + contacts[i].displayName + '：[' + '<span class="address">' + contacts[i].emails[0]["value"] + '</span>]</li>';
-        res += '<li class="mail_info">' + contacts[i].displayName + '：[' + '<span class="address">' + 'test1@gmail.com' + '</span>]</li>';
-        res += '<li class="mail_info">' + contacts[i].displayName + '：[' + '<span class="address">' + 'test2@gmail.com' + '</span>]</li>';
-    }
-    $('#msg').html(res);
-    $(".mail_info").on("click", function(){
-        alert($(this).children(".address").text());
-    });
-}
-
-=======
     // アドレス情報を表示
     $("#address_setting_link").click(function() {
         var fstr = "";
@@ -50,7 +24,6 @@ function onSuccess(contacts) {
 }
 
 // アドレス取得失敗
->>>>>>> Stashed changes
 function onError() {
     alert('Error');
 }
