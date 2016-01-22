8b4b96b64f20de6359c43da520f1532d0cb7a955
t-nakahata <t_nakahata@RJC7052.r-jc.local>
2016-01-22 13:19:37 +0900

    refs #8251アドレス 連絡先アプリ連係部品

--- app/src/main/assets/www/js/contact.js
+++ app/src/main/assets/www/js/contact.js
@@ -1,3 +1,27 @@
 document.addEventListener('deviceready', function() {
     // 連絡先機能を実装してください。
+    $(".address").live("click", function(){
+        alert("clickされたよ");
+    });
 }, false);
+
+function doAction(){
+    var fstr = document.getElementById('input').value;
+    var opt = new ContactFindOptions();
+    opt.filter = fstr;
+    var flds = ["displayName","emails"];
+    var contact = navigator.contacts.find(flds,onSuccess,onError,opt);
+}
+
+function onSuccess(contacts) {
+    var res = "<ol>";
+    for (var i=0; i < contacts.length; i++) {
+        res += '<li><span class="address">' + contacts[i].displayName + '：' + contacts[i].emails[0]["value"] + '</span></li>';
+    }
+    res += "<ol>";
+    document.getElementById('msg').innerHTML = res;
+}
+
+function onError() {
+    alert('Error');
+}
