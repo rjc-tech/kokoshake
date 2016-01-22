e2cda7925e9172f50509d2a30dae02d24ebc1258
Toshiya Sugano <t_sugano@r-jc.jp>
2016-01-22 18:29:52 +0900

    refs #8519 サンプルソースの格納(動作未確認)

--- app/src/main/assets/www/js/shake.js
+++ app/src/main/assets/www/js/shake.js
@@ -1,3 +1,47 @@
+var geoOptions = { maximumAge: 3000, timeout: 10000, enableHighAccuracy: true };
+var lastShaked = 0;
+
 document.addEventListener('deviceready', function() {
-    // GPS・加速度の処理を記述してください。
+
+    // ======== GPS機能 =========
+    // navigator.geolocationの利用可否のチェック
+    if (navigator.geolocation){
+        function success(pos) {
+            var crd = pos.coords;
+
+            var latitude = crd.latitude;
+            var longitude = crd.longitude;
+
+            // TODO 取得した値どう渡すかなー？
+        };
+
+        function error(err) {
+            console.warn('ERROR(' + err.code + '): ' + err.message);
+        };
+
+        navigator.geolocation.getCurrentPosition(success, error, geoOptions);
+    } else {
+        // TODO アラート表示どうすんじゃー
+        console.log("つかえないよー");
+    }
+
+    // ======== シェイク判定機能 =========
+    // シェイクイベント利用可否チェック
+    if (window.DeviceMotionEvent) {
+        window.addEventListener("devicemotion", function(event){
+            var x = event.accelerationIncludingGravity.x;
+
+            if (x > 10) {
+                // TODO とりあえず1秒間ロック
+                if (new Date().getTime() - lastShaked > 1000) {
+                    lastShaked = new Date().getTime();
+                    // TODO シェイク判定OKなので、なんとかして結果を返す
+                }
+            }
+        }, true);
+    } else {
+        // TODO アラート表示どうすんじゃー
+        console.log("つかえないよー");
+    }
+
 }, false);
